f16414d48a5a30aa3208301b69dd4e9212a12389
KylePac <147895229+km-pac@users.noreply.github.com>
2023-11-30 12:07:19 +0800

    Create momonscript.py
    test

---
+++ momonscript.py
@@ -0,0 +1,22 @@
+text1 = open("myFile.txt","w")
+LinesText1 = text.readlines()
+
+for line in Lines:
+  print(line)
+text1.close()
+
+
+
+
+# # CLEANING THE FIRST FILE FROM TWMON
+# # Read in the file
+# with open('file.txt', 'r') as file:
+#   filedata = file.read()
+
+# # Replace the target string
+# filedata = filedata.replace('No new IP', '')
+# filedata = filedata.replace('No new IP', '')
+
+# # Write the file out again
+# with open('file.txt', 'w') as file:
+#   file.write(filedata)

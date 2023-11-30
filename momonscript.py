2a9361d66b9fda3839ca81733ad9e959f3cf0d74
KylePac <147895229+km-pac@users.noreply.github.com>
2023-11-30 13:30:31 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -2,7 +2,10 @@
 with open('file1.txt', 'r') as file1:
   filedata = file1.read()
   rem_ip = filedata.replace('No new IP', '')
-  rem_space = rem_ip.strip()
+
+  lines = file1.readlines()
+  for line in lines:
+    rem_space = rem_ip.replace(' ','')
 
 with open('file1.txt', 'w') as file1:
   file1.write(rem_space)

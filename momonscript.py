5f852a5bd33776da8c9e54bdb5ae1e5f0560338a
KylePac <147895229+km-pac@users.noreply.github.com>
2023-11-30 16:11:05 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -1,17 +1,18 @@
 # CLEANING THE FIRST FILE FROM TWMON SHEET
 with open('file1.txt', 'r') as file1:
+  cleaned_iplist_twmon = []
   iplist = file1.readlines()
-
-  cleaned_iplist = []
   for line in iplist:
       rem_ip = line.replace('No new IP', '').replace('\n', '')
       if rem_ip != '':
-          cleaned_iplist.append(rem_ip)
+          cleaned_iplist_twmon.append(rem_ip)
 
   
 with open('file2.txt', 'r') as file2:
+    cleaned_iplist_momon = []
     dataline = file2.read()
-    cleaned_iplist_2 = dataline.split(",")
+    iplist = dataline.replace('[','').replace('"','').replace('\\','').split(",")
+  
 
     for line in cleaned_iplist_2:
       print(line)

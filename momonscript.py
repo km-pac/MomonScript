cd2ea060433e5be099fbe8a8e3f6936a2a2cd925
KylePac <147895229+km-pac@users.noreply.github.com>
2023-11-30 16:16:26 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -12,10 +12,14 @@ with open('file2.txt', 'r') as file2:
     cleaned_iplist_momon = []
     dataline = file2.read()
     iplist = dataline.replace('[','').replace('"','').replace('\\','').split(",")
-  
-
-    for line in cleaned_iplist_momon:
+   
+    for line in iplist:  
       print(line)
+      # if line.isdigit() || line:
+      #   cleaned_iplist_momon.append(line)
+
+    
+        
 
 file1.close()
 file2.close()

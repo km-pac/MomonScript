71bbfb9c7380e9eac5535fc93c625409a68faac1
KylePac <147895229+km-pac@users.noreply.github.com>
2023-11-30 12:32:49 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -1,12 +1,8 @@
 # CLEANING THE FIRST FILE FROM TWMON
-# Read in the file
 with open('file1.txt', 'r') as file1:
   filedata = file1.read()
+  rem_ip = filedata.replace('No new IP', '')
+  rem_space = rem_ip.replace(' ', '\r')
 
-# Replace the target string
-rem_ip = filedata.replace('No new IP', '')
-rem_space = rem_ip.replace('.', '|')
-
-# Write the file out again
 with open('file1.txt', 'w') as file1:
   file1.write(rem_space)

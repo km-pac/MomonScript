bf18db2f12bc89ebfa9289b1b937cf3fa9e72929
KylePac <147895229+km-pac@users.noreply.github.com>
2023-11-30 16:16:54 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -11,7 +11,7 @@ with open('file1.txt', 'r') as file1:
 with open('file2.txt', 'r') as file2:
     cleaned_iplist_momon = []
     dataline = file2.read()
-    iplist = dataline.replace('[','').replace('"','').replace('\\','').split(",")
+    iplist = dataline.replace('[','').replace(']','').replace('"','').replace('\\','').split(",")
    
     for line in iplist:  
       print(line)

5890512045740b6707afa2f7f2c9e31557640a6b
KylePac <147895229+km-pac@users.noreply.github.com>
2023-11-30 15:44:08 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -8,8 +8,10 @@ with open('file1.txt', 'r') as file1:
   iplist = file1.readlines()
   for line in iplist:
     rem_ip = line.replace('No new IP', '')
-    rem_space = rem_ip.replace('\n', ' ')
-    a_iplist.append(rem_space)
+    rem_space = rem_ip.replace('\n', '')
+    if line != " ":
+      a_iplist.append(rem_space)
+    
   
   print(a_iplist)
   

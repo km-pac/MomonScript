c00e307e402abb1244de85c3de221296ecc8f827
KylePac <147895229+km-pac@users.noreply.github.com>
2023-12-01 09:59:22 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -1,36 +1,38 @@
-# CLEANING THE FIRST FILE FROM TWMON SHEET
-with open('twmon.txt', 'r') as file1:
-  cleaned_iplist_twmon = []
-  iplist = file1.readlines()  
-  for line in iplist:
-      rem_ip = line.upper().replace("SUBNET").replace('NO NEW IP', '').replace('\n', '')
-      if rem_ip != '':
-          cleaned_iplist_twmon.append(rem_ip)
+# # CLEANING THE FIRST FILE FROM TWMON SHEET
+# with open('twmon.txt', 'r') as file1:
+#   cleaned_iplist_twmon = []
+#   iplist = file1.readlines()  
+#   for line in iplist:
+#       rem_ip = line.upper().replace("SUBNET").replace('NO NEW IP', '').replace('\n', '')
+#       if rem_ip != '':
+#           cleaned_iplist_twmon.append(rem_ip)
 
   
-with open('momon.txt', 'r') as file2:
-    cleaned_iplist_momon = []
-    dataline = file2.read()
-    iplist = dataline.replace('[','').replace(']','').replace('"','').replace('\\','').split(",")
-    cleaned_iplist_momon = [ip for ip in iplist if "/" in ip]
+# with open('momon.txt', 'r') as file2:
+#     cleaned_iplist_momon = []
+#     dataline = file2.read()
+#     iplist = dataline.replace('[','').replace(']','').replace('"','').replace('\\','').split(",")
+#     cleaned_iplist_momon = [ip for ip in iplist if "/" in ip]
 
-file1.close()
-file2.close()
+# file1.close()
+# file2.close()
 
-# print(cleaned_iplist_twmon)
-# print(cleaned_iplist_momon)
+# # print(cleaned_iplist_twmon)
+# # print(cleaned_iplist_momon)
 
-# print("TWMON")
-# for line in cleaned_iplist_twmon:
+# # print("TWMON")
+# # for line in cleaned_iplist_twmon:
+# #   print(line)
+# # print("\n\n\nMOMON")
+# # for line in cleaned_iplist_momon:
+# #   print(line)
+
+# new_ips = [element for element in cleaned_iplist_momon if element not in cleaned_iplist_twmon]
+
+# print("NEW IP ADDRESSES")
+# for line in new_ips:
 #   print(line)
-# print("\n\n\nMOMON")
-# for line in cleaned_iplist_momon:
-#   print(line)
 
-new_ips = [element for element in cleaned_iplist_momon if element not in cleaned_iplist_twmon]
-
-print("NEW IP ADDRESSES")
-for line in new_ips:
-  print(line)
-
-
+from scapy.all import *
+target = ["192.168.1.254"]
+result, unans = traceroute(target,maxttl=32)

225e95679bb6fd164dc8770aec499de7a55596f8
KylePac <147895229+km-pac@users.noreply.github.com>
2023-12-01 14:37:10 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -1,22 +1,26 @@
+import pexpect
+
 # # CLEANING THE FIRST FILE FROM TWMON SHEET
-# with open('twmon.txt', 'r') as file1:
-#   cleaned_iplist_twmon = []
-#   iplist = file1.readlines()  
-#   for line in iplist:
-#       rem_ip = line.upper().replace("SUBNET").replace('NO NEW IP', '').replace('\n', '')
-#       if rem_ip != '':
-#           cleaned_iplist_twmon.append(rem_ip)
+with open('twmon.txt', 'r') as file1:
+  cleaned_iplist_twmon = []
+  iplist = file1.readlines()  
+  for line in iplist:
+      rem_ip = line.upper().replace("SUBNET").replace('NO NEW IP', '').replace('\n', '')
+      if rem_ip != '':
+          cleaned_iplist_twmon.append(rem_ip)
 
-  
-# with open('momon.txt', 'r') as file2:
-#     cleaned_iplist_momon = []
-#     dataline = file2.read()
-#     iplist = dataline.replace('[','').replace(']','').replace('"','').replace('\\','').split(",")
-#     cleaned_iplist_momon = [ip for ip in iplist if "/" in ip]
+# # CLEANING THE FIRST FILE FROM MOMON SHEET
+with open('momon.txt', 'r') as file2:
+    cleaned_iplist_momon = []
+    dataline = file2.read()
+    iplist = dataline.replace('[','').replace(']','').replace('"','').replace('\\','').split(",")
+    cleaned_iplist_momon = [ip for ip in iplist if "/" in ip]
 
-# file1.close()
-# file2.close()
+# # CLOSE BOTH FILES
+file1.close()
+file2.close()
 
+# # FOR DEBUGGING
 # # print(cleaned_iplist_twmon)
 # # print(cleaned_iplist_momon)
 
@@ -27,15 +31,14 @@
 # # for line in cleaned_iplist_momon:
 # #   print(line)
 
-# new_ips = [element for element in cleaned_iplist_momon if element not in cleaned_iplist_twmon]
+new_ips = [element for element in cleaned_iplist_momon if element not in cleaned_iplist_twmon]
 
-# print("NEW IP ADDRESSES")
-# for line in new_ips:
-#   print(line)
+print("NEW IP ADDRESSES")
+for line in new_ips:
+  print(line)
 
-import pexpect
-
-child = pexpect.spawn('ping -c 5 www.google.com')
+for 
+test = pexpect.spawn('ping -c 5 ' + )
 
 while 1:
         line = child.readline()

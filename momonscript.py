4346d17cb517c7729d165462ed82c65771fbc2e6
KylePac <147895229+km-pac@users.noreply.github.com>
2023-12-01 15:07:38 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -35,7 +35,7 @@ new_ips = [element for element in cleaned_iplist_momon if element not in cleaned
 
 print("NEW IP ADDRESSES")
 for line in new_ips:
-  modified_ip = line.split('/')[0] + '/.2'
+  modified_ip = line.split('.0/')[0] + '/.2'
   print(modified_ip)
   # ping_result = pexpect.spawn("ping -c 5 {}".format(modified_ip))
   # pingtest = ping_result.readline()

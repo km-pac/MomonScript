822204a02c22564a8ed957cbfdd298ab41895e06
KylePac <147895229+km-pac@users.noreply.github.com>
2023-12-02 14:23:17 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -44,7 +44,7 @@ for line in new_ips:
               # print("HOP: " + extracted_hop)
               hop_list.append(extracted_hop)
             except: continue
-        print(Fore.GREEN + "LAST HOP: " + hop_list[len(hop_list)-2] + Fore.WHITE)
+        print(Fore.GREEN + "LAST HOP: " + hop_list[len(hop_list)-2] + Fore.WHITE + "\n")
     else:
         print("{} is down!".format(modified_ip))
 

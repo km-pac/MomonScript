b4fe8ed604a9ba28e9bb2c5503efb9e1ce9bb6cf
KylePac <147895229+km-pac@users.noreply.github.com>
2023-12-02 16:14:30 +0800

    Update momonscript.py

--- momonscript.py
+++ momonscript.py
@@ -6,26 +6,32 @@ from urllib.request import urlopen
 from colorama import Fore, Style, init
 
 os.system("clear")
+twmon_file = "twmon.txt"
+momon_file = "momon.txt"
 
 # CLEANING THE FIRST FILE FROM TWMON SHEET
-with open('twmon.txt', 'r') as file1:
-  cleaned_iplist_twmon = []
-  iplist = file1.readlines()  
-  for line in iplist:
-      rem_ip = line.upper().replace("SUBNET", '').replace('NO NEW IP', '').replace('\n', '')
-      if rem_ip != '':
-          cleaned_iplist_twmon.append(rem_ip)
+def extract_twmon_ip(twmon_file):
+  with open('twmon.txt', 'r') as file1:
+    cleaned_iplist_twmon = []
+    iplist = file1.readlines()  
+    for line in iplist:
+        rem_ip = line.upper().replace("SUBNET", '').replace('NO NEW IP', '').replace('\n', '')
+        if rem_ip != '':
+            cleaned_iplist_twmon.append(rem_ip)
+  file1.close()
+  return cleaned_iplist_twmon
 
 # CLEANING THE FIRST FILE FROM MOMON SHEET
-with open('momon.txt', 'r') as file2:
-    cleaned_iplist_momon = []
-    dataline = file2.read()
-    iplist = dataline.replace('[','').replace(']','').replace('"','').replace('\\','').split(",")
-    cleaned_iplist_momon = [ip for ip in iplist if "/" in ip]
+def extract_momon_ip(momon_file):
+  with open('momon.txt', 'r') as file2:
+      cleaned_iplist_momon = []
+      dataline = file2.read()
+      iplist = dataline.replace('[','').replace(']','').replace('"','').replace('\\','').split(",")
+      cleaned_iplist_momon = [ip for ip in iplist if "/" in ip]
+  file2.close()
+  return cleaned_iplist_momon
 
-# CLOSE BOTH FILES
-file1.close()
-file2.close()
+
 
 new_ips = [element for element in cleaned_iplist_momon if element not in cleaned_iplist_twmon]
 
